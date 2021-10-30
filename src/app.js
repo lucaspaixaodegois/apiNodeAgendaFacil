6988a0a50344757e0faebf5b8c4e7b8ca904c0c7
LUCAS FRANCISCO PAIXÃO DE GOIS <lucaspaixaodegois>
2021-10-30 12:40:36 -0300

    ADD: put, editar usuario , passando id e dados pelo body

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require('express');//importando o express
+const mongoose = require('mongoose');//importando o mongoose para conexão com banc
 const res = require('express/lib/response');
-const mongoose = require('mongoose');
 
 require("./models/Usuario");
 const Usuario = mongoose.model('usuario');
 
 const app = express();
-
+//habilitando o uso do json
 app.use(express.json());
-
+//conexao com mongodb atlas
 mongoose.connect('mongodb+srv://root:root@clusterdeploy.hl91e.mongodb.net/agendaFacil', {
   useNewUrlParser: true,
   useUnifiedTopoLogy: true
@@ -18,23 +18,6 @@ mongoose.connect('mongodb+srv://root:root@clusterdeploy.hl91e.mongodb.net/agenda
   console.log("Erro: A conexão com MongoDB não foi realizada com sucesso!");
 });
 
-//buscar por id
-app.get("/usuario/:id", (req, res) => {
-
-  Usuario.findById(req.params.id ).then((usuario) => {
-    return res.json(usuario);
-  }).catch((erro) => {
-    return res.status(400).json({
-      error: true,
-      menssage: "Nenhum usuário encotrado!"
-    })
-  })
-
-
-
-  // return res.json({ id: req.params.id })
-
-});
 
 //listar todos usuarios 
 app.get("/", (req, res) => {
@@ -43,7 +26,7 @@ app.get("/", (req, res) => {
   }).catch((erro) => {
     return res.status(400).json({
       error: true,
-      menssage: "Nenhum usuário encotrado!"
+      message: "Nenhum usuário encotrado!"
     })
   })
 
@@ -54,7 +37,7 @@ app.post("/usuario", (req, res) => {
   const usuario = Usuario.create(req.body, (err) => {
     if (err) return res.status(400).json({
       error: true,
-      menssage: "Error: Usuário nao foi cadastrado com sucesso!"
+      message: "Error: Usuário nao foi cadastrado com sucesso!"
     })
 
     return res.status(201).json({
@@ -65,7 +48,40 @@ app.post("/usuario", (req, res) => {
   })
 });
 
+//buscar por id
+app.get("/usuario/:id", (req, res) => {
+
+  Usuario.findById(req.params.id).then((usuario) => {
+    return res.json(usuario);
+  }).catch((erro) => {
+    return res.status(400).json({
+      error: true,
+      message: "Nenhum usuário encotrado!"
+    })
+  })
+
+});
+//editar usuario
+app.put("/usuario/:id", (req, res) => {
+
+  const usuario = Usuario.updateOne({ _id: req.params.id }, req.body, (err) => {
+    if (err) return res.status(400).json({
+      error: true,
+      message: "Error:Usuário não foi editado com sucesso!"
+    });
+
+    return res.json({
+      error: false,
+      message: "Usuário editado com sucesso!",
+    });
+  });
+});
+
+
+
 app.listen(8080, () => {
   console.log(" Servidor iniciado na porta 8080: http:localhost:8080/")
 
 });
+
+
